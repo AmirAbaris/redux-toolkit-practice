1fd91e9f942f3a893c3f73d32d325059966dba61
Amir Abaris <thisisamirabaris@gmail.com>
2025-01-24 01:45:32 +0330

    implemented the basic structure of redux

---
+++ store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { jsonPlaceHolderApi } from "./services/jsonPlaceholderApi";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+export const store = configureStore({
+  reducer: {
+    [jsonPlaceHolderApi.reducerPath]: jsonPlaceHolderApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(jsonPlaceHolderApi.middleware),
+});
+
+setupListeners(store.dispatch);
